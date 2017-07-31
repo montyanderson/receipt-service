41bbf52feefe8df265bec35841d7e2a4553d6886
Monty Anderson <monty.linux@gmail.com>
2017-07-31 12:23:53 +0100

    supports smaller text

--- routes/order.js
+++ routes/order.js
@@ -3,17 +3,63 @@ const lp = require("../lib/lp");
 
 const heading = Buffer.from([ 27, 33, 32 ]);
 const plain = Buffer.from([ 27, 33, 0 ]);
+const smaller = Buffer.from([ 27, 33, 1 ]);
+
+function centerPad(text, width = 32) {
+	text = text.toString();
+
+	const pad = " ".repeat((width - text.length) / 2);
+	return `${pad}${text}${pad}\n`;
+}
+
+function rightPad(text, width = 32) {
+	text = text.toString();
+
+	const pad = " ".repeat(width - text.length);
+	return `${pad}${text}\n`;
+}
+
+function format(arr, width = 32) {
+	const totalPadding = arr
+		.map(a => a.toString())
+		.reduce((a, b) => a + b.length, 0);
+
+	const pad = " ".repeat((width - totalPadding) / (arr.length - 1));
+	return arr.join(pad) + "\n";
+}
+
+function textWrap(text, width = 32) {
+	text = text.toString();
+
+	for(let i = width - 1; i < text.length; i += width) {
+		while(text[i] != " ") {
+			i--;
+		}
+
+		text = text.slice(0, i) + "\n" + text.slice(i + 1);
+	}
+
+	return text;
+}
 
 module.exports = _.post("/order", async ctx => {
-	const merchant = ctx.request.body.merchant.slice(0, 16);
-	const merchantPadding = " ".repeat((16 - merchant.length) / 2);
+	const { body } = ctx.request;
 
-	lp.write(`${heading}${merchantPadding}${merchant}${merchantPadding}\n\n`)
+	lp.write(`\n${heading}${centerPad(body.merchant, 16)}\n`);
+	lp.write(`${plain}${centerPad(body.date)}`);
+	lp.write(`${plain}${centerPad(body.order)}\n`);
 
-	for(let item of ctx.request.body.items) {
+	for(let item of body.items) {
 		const price = item.price.toString();
 		const name = item.name.slice(0, 32 - price.length - 1);
 
-		lp.write(`${plain}${name}${" ".repeat(32 - (name.length + price.length))}${price}\n`);
+		lp.write(`${plain}${format([ name, price ])}`);
 	}
+
+	const total = body.total.toString();
+
+	lp.write(`${plain}${"-".repeat(32)}`);
+	lp.write(`${plain}${format([ "Total", total ])}`);
+	lp.write(`${plain}${"-".repeat(32)}\n\n`);
+	lp.write(`${smaller}${textWrap(body.text, 42)}\n`);
 });
